42918580b1dbd094fccb3c522f34fae1bed16699
RomashinTimofey <bootybayfur@mail.ru>
2017-12-01 15:23:51 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+*	Class Класс для вывода сообщения в консоль
+*	@author tromashin
+*	@since 01.12.2017
+*	@version 1
+	*/
 public class Calculate {
+	
+/**
+*	@param args сюда придут параметры с командной строки с которыми *	* 	программа была запущена в виде массива строк	
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
