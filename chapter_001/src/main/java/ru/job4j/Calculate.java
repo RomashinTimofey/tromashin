5463d910f9a635f9dd34f1b68191c573b0fbc75d
RomashinTimofey <bootybayfur@mail.ru>
2017-12-01 21:22:20 +0300

    1.5. Checkstyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,28 @@
 package ru.job4j;
 
 /**
-*	Class Класс для вывода сообщения в консоль
+*	Class for out message in console
 *	@author tromashin
 *	@since 01.12.2017
 *	@version 1
-	*/
+*/
+
 public class Calculate {
 	
 /**
-*	@param args сюда придут параметры с командной строки с которыми *	* 	программа была запущена в виде массива строк	
+*	@param args String array
 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
-}
+
+/**
+* 	Method echo.
+* 	@param name Your name.
+* 	@return Echo plus your name.
+*/
+	public String echo(String name) {
+    		return "Echo, echo, echo : " + name;
+		}
+
+	}
