7aa80efa06ed09142376cccf7723aff72f0626bd
RomashinTimofey <bootybayfur@mail.ru>
2017-12-03 23:24:42 +0300

    1.5 (Fixed)CheckStyle added repository

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,28 +1,26 @@
 package ru.job4j;
 
 /**
-*	Class for out message in console
-*	@author tromashin
-*	@since 01.12.2017
-*	@version 1
-*/
+ *	Class for out message in console
+ *	@author tromashin
+ *	@since 01.12.2017
+ *	@version 1
+ */
 
 public class Calculate {
-	
-/**
-*	@param args String array
-*/
+
+	/**
+	 *	@param args String array
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 
-/**
-* 	Method echo.
-* 	@param name Your name.
-* 	@return Echo plus your name.
-*/
+	/**
+	 * 	Method echo.
+	 * 	@param name Your name.
+	 * 	@return Echo plus your name.
+	 */
 	public String echo(String name) {
-    		return "Echo, echo, echo : " + name;
-		}
-
-	}
+		return "Echo, echo, echo : " + name; }
+}
