4dc2f8e697005397169fb3505a8f88058c1d02eb
RomashinTimofey <bootybayfur@mail.ru>
2017-12-09 16:11:30 +0300

    4.3. Построить шахматную доску в псевдографике.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+
+/**
+ * Class for paint Chess Board
+ * @author Timofey Romashin
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+
+    /**
+     *
+     * @param width of ChessBoard
+     * @param height of ChessBoard
+     * @return painted ChessBoard
+     */
+    public String paint(int width, int height) {
+        StringBuilder screenBuilder = new StringBuilder();
+        boolean state = true;
+        String ln = System.lineSeparator();
+        for (int heightCounter = 0; heightCounter < height; heightCounter++) {
+            for (int widthCounter = 0; widthCounter < width; widthCounter++) {
+                if (state) {
+                    screenBuilder.append("X");
+                } else {
+                    screenBuilder.append(" ");
+                }
+                state = !state;
+            }
+            screenBuilder.append(ln);
+        }
+        return screenBuilder.toString();
+    }
+}
