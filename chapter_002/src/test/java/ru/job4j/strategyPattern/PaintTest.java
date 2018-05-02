eeb82cc14cef6dfa45f7530505122b182e777358
RomashinTimofey <bootybayfur@mail.ru>
2018-05-02 17:17:59 +0300

    5. Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/strategyPattern/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategyPattern/PaintTest.java
@@ -1,20 +1,31 @@
 package ru.job4j.strategyPattern;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфер для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
         // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
         // результат ожидания
@@ -31,7 +42,65 @@ public class PaintTest {
                 .toString();
         // проверяем результат вычисления
         assertThat(new String(out.toByteArray()), is(result));
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
+
     }
+
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        String ln = System.lineSeparator();
+        String result = new StringBuilder()
+                .append("+")
+                .append(ln)
+                .append("+ +")
+                .append(ln)
+                .append("+  +")
+                .append(ln)
+                .append("+++++")
+                .append(ln)
+                .toString();
+        assertThat(new String(this.out.toByteArray()), is(result));
+    }
+
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+//    @Test
+//    public void whenDrawSquare() {
+//        new Paint().draw(new Square());
+//        assertThat(
+//                new String(this.out.toByteArray()),
+//                is(
+//                        new StringBuilder()
+//                                .append("++++")
+//                                .append("+     +")
+//                                .append("+     +")
+//                                .append("++++")
+//                                .append(System.lineSeparator())
+//                                .toString()
+//                )
+//        );
+//    }
+//
+//    @Test
+//    public void whenDrawTriangle() {
+//        new Paint().draw(new Triangle());
+//        assertThat(
+//                new String(this.out.toByteArray()),
+//                is(
+//                        new StringBuilder(
+//                                System.lineSeparator(), "",
+//                                System.lineSeparator())
+//                                .add("  ^  ")
+//                                .add(" ^ ^ ")
+//                                .add("^^^^^")
+//                                .toString()
+//                )
+//        );
+//    }
 }
